4b50153892d0cd0d9277ba6465f98ff420e945f8
Soliman-soad <mdsolimansoad@gmail.com>
2022-11-24 01:07:31 +0600

    footer section added

--- src/pages/layout/Main.jsx
+++ src/pages/layout/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../shared/Footer';
 import Navber from '../shared/Navber';
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
         <div>
             <Navber/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
---
+++ src/pages/shared/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="px-4 py-8 dark:bg-gray-800 dark:text-gray-400">
+	<div className="container flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
+		<div className="flex flex-row pr-3 space-x-4 sm:space-x-8">
+			<ul className="flex flex-wrap items-center space-x-4 sm:space-x-8">
+				<li>
+					<p>All rights and condition preserved by Soliman</p>
+				</li>
+				
+			</ul>
+		</div>
+		<ul className="flex flex-wrap pl-3 space-x-4 sm:space-x-8">
+			<li>
+				<a href="#">Instagram</a>
+			</li>
+			<li>
+				<a href="#">Facebook</a>
+			</li>
+			<li>
+				<a href="#">Twitter</a>
+			</li>
+		</ul>
+	</div>
+</footer>
+        </div>
+    );
+};
+
+export default Footer;
